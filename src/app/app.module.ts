81785785c2b38562b5a114b85a024cfccd13874d
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-12-12 23:55:34 +0200

    feat: Generated search-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchInputComponent } from './search-input/search-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
