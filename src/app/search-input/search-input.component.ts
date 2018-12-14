feb049e8159384e2a83a47bfa85cbeff1f9ecd69
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-12-14 23:25:15 +0200

    Output the search value to app

--- src/app/search-input/search-input.component.ts
+++ src/app/search-input/search-input.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-input',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchInputComponent implements OnInit {
 
+  @ViewChild('f') recipe: NgForm;
+  @Output() InputValue: any = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.InputValue.emit(this.recipe.value.searchRecipe);
+    console.log(this.recipe.value.searchRecipe);
+  }
 }
