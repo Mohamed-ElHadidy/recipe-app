fbd3f03851904993cf1e98393793889cfe6be391
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-12-15 23:38:01 +0200

    added APIURL, key and Id

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
-  private app_key: string = '';
-  private app_id: string = '';
-  private querry: string = '';
-  private url: string = '';
+  private app_key: string = '056c238ee6d7e2f113089e04e8a43064';
+  private app_id: string = '057fc72b';
+  private url: string = 'https://api.edamam.com/search?';
 
   constructor( private http: HttpClient ) { }
 
   getRecipesData(querry: string): Observable<any> {
-    return this.http.get(this.url);
+    const fullUrl = `${this.url}q=${querry}&app_id=${this.app_id}&app_key=${this.app_key}`;
+    return this.http.get(fullUrl);
   }
 
 }
